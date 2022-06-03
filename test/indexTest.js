854d4e65061e246722087372a9b594cb7d6c9cff
Waithera Munderu <wmunderu@gmail.com>
2022-06-03 08:05:53 +0300

    commit tree b30d411efaa5d9cba7d095d4ce332c34076c666b

--- test/indexTest.js
+++ test/indexTest.js
@@ -9,14 +9,14 @@ describe('employees', function() {
     });
 
     it('returns an employee with the original key value pairs and the new key value pair', function () {
-      expect(updateEmployeeWithKeyAndValue(employee, 'streetAddress', '11 Broadway')).to.eql({
+      expect(updateEmployeeWithKeyAndValue(employee, 'streetAddress', 'Kimathi Highway')).to.eql({
         name: 'Sam',
-        streetAddress: '11 Broadway'
+        streetAddress: 'Kimathi Highway'
       });
     });
 
     it('it does not modify the original employee, but rather returns a clone with the new data', function () {
-      updateEmployeeWithKeyAndValue(employee, 'streetAddress', '11 Broadway');
+      updateEmployeeWithKeyAndValue(employee, 'streetAddress', 'Kimathi Highway');
 
       expect(employee['streetAddress']).to.equal(undefined);
     });
@@ -24,14 +24,14 @@ describe('employees', function() {
 
   describe('destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value)', function () {
     it('updates `employee` with the given `key` and `value` (it is destructive) and returns the entire updated employee', function () {
-      expect(destructivelyUpdateEmployeeWithKeyAndValue(employee, 'streetAddress', '12 Broadway')).to.eql({
+      expect(destructivelyUpdateEmployeeWithKeyAndValue(employee, 'streetAddress', 'Kimathi Highway')).to.eql({
         name: 'Sam',
-        streetAddress: '12 Broadway'
+        streetAddress: 'Kimathi Highway'
       });
 
       expect(employee).to.eql({
         name: 'Sam',
-        streetAddress: '12 Broadway'
+        streetAddress: 'Kimathi Highway'
       });
     });
   });
